48384996a12f87ccdff8cd9146b7aed9b88dafeb
Dv <75692623+kyaruwo@users.noreply.github.com>
2023-06-13 11:02:54 +0800

    Update status.py

--- game/status.py
+++ game/status.py
@@ -1,2 +1,18 @@
 def status():
-    return
+    from main import title, line
+    from lib import DB
+
+    (db, c) = DB.connect()
+    (atk) = c.execute("").fetchone()
+    (lvl, xp, gold) = c.execute("select * from mages").fetchone()
+    db.close()
+
+    title()
+    print("     atk", atk)
+    print("     lvl", lvl)
+    print("      xp", xp)
+    print("       g", gold)
+    line()
+    print("     <?> return")
+    line()
+    input(" >>>> ")

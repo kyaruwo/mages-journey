49d5011f7e9badedc613d97014d7f41896edf9f3
Dv <75692623+kyaruwo@users.noreply.github.com>
2023-06-13 21:54:24 +0800

    Update status.py

--- game/status.py
+++ game/status.py
@@ -1,19 +1,43 @@
-def status():
-    from main import line
+def magestats():
     from lib import DB
 
     (db, c) = DB.connect()
-    (atk) = c.execute("").fetchone()
+
     (lvl, xp, gold) = c.execute("select * from mages").fetchone()
+
+    hp = int(6 + lvl)
+
+    baseatk = int(3 + lvl)
+    (atk) = c.execute(
+        """
+    select sum(atk*count)
+    from inventory join market
+    on itemid = market.ROWID
+        """
+    ).fetchone()[0]
+    if atk != None:
+        atk += baseatk
+    else:
+        atk = baseatk
+
     db.close()
+
+    return (lvl, xp, gold, hp, atk)
+
+
+def status():
+    from main import line
+
+    (lvl, xp, gold, hp, atk) = magestats()
 
     line()
     print((8 * " ") + "STATUS")
     line()
-    print("     atk", atk)
     print("     lvl", lvl)
     print("      xp", xp)
     print("       g", gold)
+    print("      hp", hp)
+    print("     atk", atk)
     line()
     print("     <?> return")
     line()

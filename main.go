50bde576b13ea8c968aea2c5a5ff4f86b2b1aeb4
kyaruwo <75692623+kyaruwo@users.noreply.github.com>
2024-02-01 11:18:19 +0800

    Update main.go

--- main.go
+++ main.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
 )
 
 func main() {
+	clear := exec.Command("cmd", "/c", "cls")
+	clear.Stdout = os.Stdout
+	clear.Run()
+
 	fmt.Println("=o= mages journey =o=")
 }

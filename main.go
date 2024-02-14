c4789885400e62bcd4c6788dc47b7876d26ae47e
kyaruwo <75692623+kyaruwo@users.noreply.github.com>
2024-02-14 12:02:46 +0800

    Update main.go

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ func main() {
 	// todo; fetch from database
 	var lvl, xp, g, hp, atk = 1, 0, 0, 8, 4
 
-	fmt.Println("\n      - stats -")
+	fmt.Println("\n      - stats -        ")
 	fmt.Println("    lvl", lvl, "   xp", xp)
 	fmt.Println("     hp", hp, "  atk", atk)
 	fmt.Println("      g", g)

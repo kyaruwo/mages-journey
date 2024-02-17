ab3ad727ff0e710f8a469e3105beace468342760
kyaruwo <75692623+kyaruwo@users.noreply.github.com>
2024-02-17 12:23:13 +0800

    Update main.go

--- main.go
+++ main.go
@@ -7,12 +7,29 @@ import (
 )
 
 func main() {
+	title()
+
+	fmt.Println("\n     0 - status")
+
+	fmt.Print("\n>>>> ")
+	var op int
+	fmt.Scan(&op)
+
+	switch op {
+	case 0:
+		status()
+	}
+}
+
+func title() {
 	var clear = exec.Command("cmd", "/c", "cls")
 	clear.Stdout = os.Stdout
 	clear.Run()
-
 	fmt.Println("=o= mages journey =o=")
+}
 
+func status() {
+	title()
 	// todo; fetch from database
 	var lvl, xp, g, hp, atk = 1, 0, 0, 8, 4
 
@@ -20,8 +37,4 @@ func main() {
 	fmt.Println("    lvl", lvl, "   xp", xp)
 	fmt.Println("     hp", hp, "  atk", atk)
 	fmt.Println("      g", g)
-
-	fmt.Print("\n\n >>>> ")
-	var op string
-	fmt.Scan(&op)
 }

51c38bd64ef7fc4eb867f9d7c837222032a388e1
kyaruwo <75692623+kyaruwo@users.noreply.github.com>
2024-03-22 19:49:26 +0800

    Update status.go

--- game/status.go
+++ game/status.go
@@ -3,14 +3,43 @@ package game
 import (
 	"database/sql"
 	"fmt"
+	"log"
 	"mages-journey/gamemod"
 )
 
 func Status(db *sql.DB) {
 	gamemod.Title()
 
-	// todo; fetch from database
-	var lvl, xp, g, hp, atk = 1, 0, 0, 8, 4
+	var id, lvl, xp, g int
+	err := db.QueryRow(`
+	select
+		rowid, lvl, xp, gold
+	from
+		mages
+	`).Scan(&id, &lvl, &xp, &g)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var hp = 6 + lvl
+
+	var atk = 3 + lvl
+	var item_atk sql.NullInt64
+	err = db.QueryRow(`
+	select
+		sum(atk * count)
+	from
+		inventory
+		join market on item_id = market.ROWID
+	where
+		mage_id = ?
+	`, id).Scan(&item_atk)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if item_atk.Valid {
+		atk = atk + int(item_atk.Int64)
+	}
 
 	fmt.Println("\n     - status -")
 	fmt.Println("   lvl", lvl, "   xp", xp)
